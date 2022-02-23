933eb2b9b9563a080553abc8a3c2f7f7136b8598
chlee7139 <chlee7139@gmail.com>
2022-02-23 09:47:41 +0900

    테스트

--- src/main/kotlin/io/github/plus/Main.kt
+++ src/main/kotlin/io/github/plus/Main.kt
@@ -7,10 +7,10 @@ import org.bukkit.plugin.java.JavaPlugin
 class Main : JavaPlugin(), Listener {
 
     override fun onEnable() {
-        server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 활성화 되었습니다.")
+        server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 활성화 되었습니다!")
     }
     override fun onDisable() {
-        server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 활성화 되었습니다.")
+        server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 활성화 되었습니다!")
     }
 
 }
