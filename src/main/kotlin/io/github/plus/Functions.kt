fa7654054c04b19df6848cf5fd4577b023b04763
BuildTools <unconfigured@null.spigotmc.org>
2022-02-23 10:41:12 +0900

    수정

--- src/main/kotlin/io/github/plus/Functions.kt
+++ src/main/kotlin/io/github/plus/Functions.kt
@@ -1,10 +1,11 @@
 package io.github.plus
 
+import net.kyori.adventure.text.Component
 import net.md_5.bungee.api.ChatMessageType
-import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.plugin.Plugin
 import org.bukkit.scheduler.BukkitRunnable
+import java.awt.TextComponent
 
 class Functions {
     fun runTaskTimer(plugin : Plugin?, delay : Long, period : Long, task : () -> Unit): BukkitRunnable {
@@ -35,5 +36,10 @@ class Functions {
         return rr
 
     }
-    fun Player.sendActionBar(text : String) = this.spigot().sendMessage(ChatMessageType.ACTION_BAR, net.md_5.bungee.api.chat.TextComponent(text))
+    fun Player.sendActionBar(text : String) {
+        val component: Component = Component.text(text)
+
+        this.sendActionBar(component)
+
+    }
 }
