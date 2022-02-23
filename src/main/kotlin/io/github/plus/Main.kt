61602542fbbe11cdda22d7d7faad05ab305f2995
enlfg2 <hchoe166@gmail.com>
2022-02-23 16:22:04 +0900

    수정

--- src/main/kotlin/io/github/plus/Main.kt
+++ src/main/kotlin/io/github/plus/Main.kt
@@ -1,8 +1,6 @@
 package io.github.plus
 
-import aa
-import io.github.plus.command.Test
-import io.github.plus.event.TestEvent
+import io.github.plus.command.gui
 import org.bukkit.ChatColor
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
@@ -11,13 +9,10 @@ class Main : JavaPlugin(), Listener {
 
     override fun onEnable() {
         server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 활성화 되었습니다.")
-        server.pluginManager.registerEvents(aa(), this@Main)
+
         //커맨드 setExecutor + setTabCompleter
-        server.getPluginCommand("test")?.setExecutor(Test())
-
-        //이벤트
-        server.pluginManager.registerEvents(TestEvent(), this)
-
+        server.getPluginCommand("test")?.setExecutor(gui())
+//ㅇㅇㅇ
     }
 
     override fun onDisable() {
