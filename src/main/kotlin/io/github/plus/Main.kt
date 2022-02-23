d9a8c87e372bf147ee8073faa9a898a1846a8c2a
BuildTools <unconfigured@null.spigotmc.org>
2022-02-23 10:06:30 +0900

    버킷러너블 사용

--- src/main/kotlin/io/github/plus/Main.kt
+++ src/main/kotlin/io/github/plus/Main.kt
@@ -3,14 +3,25 @@ package io.github.plus
 import org.bukkit.ChatColor
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
 
 class Main : JavaPlugin(), Listener {
 
     override fun onEnable() {
         server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 활성화 되었습니다.")
+        r().runTaskTimer(this, 1, 1)
     }
     override fun onDisable() {
         server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 활성화 되었습니다.")
+    }
+
+}
+
+class r: BukkitRunnable() {
+
+
+    override fun run() {
+//        실행할 구문
     }
 
 }
