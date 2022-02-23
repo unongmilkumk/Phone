fb1c8374136bac21eacc1466d0bf6bf30f8d4651
id <email>
2022-02-23 11:18:37 +0900

    수정

---
+++ src/main/kotlin/io/github/plus/Tools/Item.kt
@@ -0,0 +1,12 @@
+package io.github.plus.Tools
+
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+class Item {
+
+    fun createItemStack(type: Material, name: String) {
+        val item = ItemStack(type)
+    }
+
+}
---
+++ src/main/kotlin/io/github/plus/Tools/aa.java
@@ -0,0 +1,5 @@
+package io.github.plus.Tools;
+
+public class aa {
+
+}
