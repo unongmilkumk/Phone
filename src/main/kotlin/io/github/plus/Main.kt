088eab3f32632cb8bbad15efb1f4dab100a4c738
unongmilkumk <unongmilkumk@gmail.com>
2022-02-23 11:00:36 +0900

    Main 코드 주석 수정 + yml Description 수정

--- src/main/kotlin/io/github/plus/Main.kt
+++ src/main/kotlin/io/github/plus/Main.kt
@@ -4,17 +4,17 @@ import io.github.plus.command.Test
 import org.bukkit.ChatColor
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.scheduler.BukkitRunnable
 
 class Main : JavaPlugin(), Listener {
 
     override fun onEnable() {
         server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 활성화 되었습니다.")
-        server.getPluginCommand("test")?.setExecutor(Test())
 
+        //커맨드 setExecutor + setTabCompleter
+        server.getPluginCommand("test")?.setExecutor(Test())
     }
+
     override fun onDisable() {
         server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 비활성화 되었습니다.")
     }
-
 }
