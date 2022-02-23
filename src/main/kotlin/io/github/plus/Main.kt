3067fe1b6b5c6a97461ff02a2e9f145ca249e151
id <email>
2022-02-23 10:53:05 +0900

    수정

--- src/main/kotlin/io/github/plus/Main.kt
+++ src/main/kotlin/io/github/plus/Main.kt
@@ -4,7 +4,6 @@ import io.github.plus.command.Test
 import org.bukkit.ChatColor
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.scheduler.BukkitRunnable
 
 class Main : JavaPlugin(), Listener {
 
