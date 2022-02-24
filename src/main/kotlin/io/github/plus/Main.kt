843a6136ed298195c5367ce3807fade2e6686754
id <email>
2022-02-24 14:23:30 +0900

    수정
    Signed-off-by: id <email>

--- src/main/kotlin/io/github/plus/Main.kt
+++ src/main/kotlin/io/github/plus/Main.kt
@@ -5,15 +5,14 @@ import io.github.plus.command.Gui
 import io.github.plus.event.TestEvent
 import io.github.plus.command.CommandSample
 import io.github.plus.io.github.plus.event.CalculateExp
-import org.bukkit.Bukkit
+import io.github.plus.tools.config
 import org.bukkit.ChatColor
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
-import java.io.File
 
 class Main : JavaPlugin(), Listener {
 
-    var fil = File(Bukkit.getPluginsFolder(), "stat")
+    val config = config(this)
 
     override fun onEnable() {
         server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 활성화 되었습니다.")
@@ -30,13 +29,15 @@ class Main : JavaPlugin(), Listener {
         server.pluginManager.registerEvents(aa(), this@Main)
         server.pluginManager.registerEvents(CalculateExp(), this@Main)
 
-        if(!fil.exists()) {
-            fil.mkdir()
-        }
+        config.reloadconfig()
 
     }
 
     override fun onDisable() {
         server.consoleSender.sendMessage("${ChatColor.GREEN}플러그인이 비활성화 되었습니다.")
+        config.saveconfig()
+
     }
+
+
 }
