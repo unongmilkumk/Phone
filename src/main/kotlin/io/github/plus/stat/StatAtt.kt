55be87184904881df21b062b999b842f01f7b933
id <email>
2022-03-04 09:31:50 +0900

    데미지 표시 수정
    Signed-off-by: id <email>

--- src/main/kotlin/io/github/plus/stat/StatAtt.kt
+++ src/main/kotlin/io/github/plus/stat/StatAtt.kt
@@ -4,20 +4,27 @@ import io.github.plus.Main
 import org.bukkit.entity.*
 import org.bukkit.scheduler.BukkitRunnable
 import java.security.SecureRandom
+import kotlin.math.roundToInt
 
 class StatAtt(private val main: Main) {
     private val random: SecureRandom = SecureRandom()
     fun displayDamage(damage: Double, entity: LivingEntity) {
+
         var location = entity.eyeLocation
-        location = location.add(random.nextDouble()*2.0-1.0, random.nextDouble()*2.0-1.0, random.nextDouble()*2.0-1.0)
+        location = location.add(random.nextDouble()*2.0-1.0, random.nextDouble()*2.0+9.5, random.nextDouble()*2.0-1.0)
 
         val armorStand: ArmorStand = entity.world.spawnEntity(location, EntityType.ARMOR_STAND) as ArmorStand
 
+//        armorStand.isInvisible = true
         armorStand.isInvisible = true
-        armorStand.isMarker = true
+        armorStand.customName = "${(damage*10.0).roundToInt()/10.0}"
+        armorStand.isCustomNameVisible = true
         armorStand.setGravity(false)
+        armorStand.isMarker = true
 
-        armorStand.customName = "$damage"
+        location.y=location.y-10.0
+        println(location.y)
+        armorStand.teleport(location)
 
         class Run: BukkitRunnable() {
             override fun run() {
