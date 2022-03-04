5d3f5ecdeaa34a8c8ed92a75467b70cbbbe656dd
id <email>
2022-03-04 13:54:10 +0900

    데미지 표시 수정
    Signed-off-by: id <email>

--- src/main/kotlin/io/github/plus/stat/StatAtt.kt
+++ src/main/kotlin/io/github/plus/stat/StatAtt.kt
@@ -1,6 +1,8 @@
 package io.github.plus.stat
 
 import io.github.plus.Main
+import io.github.plus.runDelay
+import io.github.plus.runTaskTimer
 import org.bukkit.entity.*
 import org.bukkit.scheduler.BukkitRunnable
 import java.security.SecureRandom
@@ -11,29 +13,35 @@ class StatAtt(private val main: Main) {
     fun displayDamage(damage: Double, entity: LivingEntity) {
 
         var location = entity.eyeLocation
-        location = location.add(random.nextDouble()*2.0-1.0, random.nextDouble()*2.0+9.5, random.nextDouble()*2.0-1.0)
+        location = location.add(random.nextDouble()*2.0-1.0, random.nextDouble()*2.0+99.5, random.nextDouble()*2.0-1.0)
 
         val armorStand: ArmorStand = entity.world.spawnEntity(location, EntityType.ARMOR_STAND) as ArmorStand
 
-//        armorStand.isInvisible = true
+        armorStand.isInvisible = true
         armorStand.isInvisible = true
         armorStand.customName = "${(damage*10.0).roundToInt()/10.0}"
         armorStand.isCustomNameVisible = true
         armorStand.setGravity(false)
         armorStand.isMarker = true
 
-        location.y=location.y-10.0
-        println(location.y)
-        armorStand.teleport(location)
-
         class Run: BukkitRunnable() {
+            var boolean = false
             override fun run() {
-                armorStand.remove()
+                if(boolean) {
+                    armorStand.remove()
+                    cancel()
+                }
+                else {
+                    location.y=location.y-100.0
+                    println(location.y)
+                    armorStand.teleport(location)
+                    boolean = true
+                }
             }
 
         }
 
-        Run().runTaskLater(main, 30L)
+        Run().runTaskTimer(main, 2L, 28L)
 
     }
 }
